3e3ae8517fbdf71233e0eed53469bb3799573ba9
ashiouzzaman <ashiouzzaman.code@gmail.com>
2024-03-21 01:46:23 +0600

    Task 1 done

---
+++ task1.js
@@ -0,0 +1,11 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+// Function for multiply four numbers
+function fourMultiply(num1, num2, num3, num4) {
+	return num1 * num2 * num3 * num4;
+}
+
+// Calling the function for multiply four numbers
+let result = fourMultiply(10, 3, 7, 2);
+console.log(result);
