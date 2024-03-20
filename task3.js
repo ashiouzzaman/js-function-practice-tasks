f51c7fa7e6fdb25d3755bc860894d1963d8ee72c
ashiouzzaman <ashiouzzaman.code@gmail.com>
2024-03-21 02:10:19 +0600

    Task 3 done

---
+++ task3.js
@@ -0,0 +1,19 @@
+// Task-3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+// Function
+function make_avg(numbs) {
+
+	const length = numbs.length;
+	let total = 0;
+	for (let num of numbs) {
+		total = total + num;
+	}
+
+	const avg = total / length;
+	return avg;
+}
+
+// Call the function
+let numArray = [10, 30, 20, 40]
+console.log(make_avg(numArray))
